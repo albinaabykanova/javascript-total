8476a2247c1cd0ee33f9011a20be8da660ec5fa9
albinaabykanova <abykanovaalbina@gmail.com>
2023-06-13 14:49:07 +0300

    js total daily çalışması devam ediyor

---
+++ 3.Çıktı Verme/app.js
@@ -0,0 +1,42 @@
+// document write ile çıktı verme ******************************************************
+
+/*
+document.write("Hello World <br>");
+document.write("Hello Aika <br>");
+document.write("345 <br>");
+
+// console.log ile çıktı verme ******************************************************
+
+console.log("Hello World");
+console.log("Hello Aika");
+console.log("345");
+console.log(true);
+console.log({ name: "ees", surname: "baysan" });
+
+let a = 5;
+let b = 10;
+console.log("iki sayinin total : ", a + b);
+console.clear();
+
+// alert ile çıktı verme ******************************************************
+
+console.log(window); // pencereyi gösterir
+
+let u = 10;
+let t = 12;
+
+alert("total : " + (u + t));
+*/
+
+//console.log(window);
+
+//debugger
+
+/*
+   BETTER COMMENTS EXTENSION
+   ? bu bir bilgilendirme satırı
+   ! Burası çok önemlidir dikkat et
+   todo : Burası soyle yapılması gerekiyor
+   * Test mesaj..
+
+*/
---
+++ 4.Let Var Const/app.js
@@ -0,0 +1,55 @@
+// Var - Let - Const
+
+/*
+------ Scope ------ (kapsam) -----
+
+ -> Global Scope : Her yerden erişilebilir.
+ -> Function Scope
+ -> Block Scope
+
+ */
+
+// 1. Global Scope ***************
+
+// var a = 5; // var ile tanımlanan değişkenler global scope'a ait olur.
+// sen buna her yerden erirşebilirsin.
+/*
+if (true) {
+  console.log(a);
+}
+
+function method1() {
+  console.log(a);
+}
+method1();
+*/
+
+// Function Scope ***************
+// sadece tanımlandığı fonksiyon içerisinde geçerlidir. dişarıya çıkamaz. parantezlerin içinde geçerlidir.
+/*
+
+function method2() {
+  var b = 1000;
+
+  console.log(b);
+}
+method2();
+
+*/
+
+// 3. Block Scope ***************
+// blok scope sadece let ve const ile tanımlanır. var ile tanımlanamaz. function ile tanımlanamaz.
+// if, for, while, switch gibi blokların içerisinde geçerlidir.
+// sadece tanımlandığı blok içerisinde geçerlidir. dışarıya çıkamaz.
+// let ve const ile tanımlanan değişkenlerin scope'u blok scope'dur.
+
+/*
+function method1() {
+  var a = 5; // function scope
+  if (true) {
+    var b = 10; // block scope
+  }
+  while (true) {}
+  for (let i = 0; i <= 10; i++) {}
+}
+*/
