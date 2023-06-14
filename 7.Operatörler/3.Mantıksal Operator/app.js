3c7bd6152b37990f21550a3d18f21460b3100740
albinaabykanova <abykanovaalbina@gmail.com>
2023-06-14 14:59:08 +0300

    continue daily pratic

---
+++ 7.Operatörler/3.Mantıksal Operator/app.js
@@ -0,0 +1,16 @@
+//  ? -----------MATIKSAL OPERATORLER---------------- logic operators
+
+/*
+    && (AND) : VE --- TÜM KOŞULLARIN DOĞRU OLMASI GEREKİR
+    || (OR) : VEYA --- KOŞULLARDAN BİRİNİN DOĞRU OLMASI YETERLİ
+    ! (NOT) : DEGIL --- KOŞULUN DEĞİLİ DEMEK 
+*/
+
+/*
+let yas = 20;
+let para = 3500;
+let saglikliMi = false;
+let hakSayisi = 3;
+
+console.log((para > 3000 && hakSayisi > 1) || (yas > 18 && saglikliMi)); // true
+*/
---
+++ 7.Operatörler/4.Karşılaştırma Operator/app.js
@@ -0,0 +1,21 @@
+//  ?------KARŞILAŞTIRMA OPERATÖRLERİ------?
+
+/* 
+    == : Eşit mi 
+    != : Eşit değildir
+
+    >  : Büyüktür
+    <  : Küçüktür
+
+    >= : Büyük eşittir
+    <= : Küçük eşittir
+*/
+
+/*
+let hour = 12;
+let isWeekend = true;
+
+if (hour < 10 || hour > 18 || isWeekend) {
+  alert("The office is closed."); // it is the weekend
+}
+*/
