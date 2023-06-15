7df0b20386b245bf92d23d3c3500c828b1b76411
albinaabykanova <abykanovaalbina@gmail.com>
2023-06-15 13:32:48 +0300

    my daily share

---
+++ 8.Dialog Kutuları/app.js
@@ -0,0 +1,24 @@
+// ? -------- DİALOG KUTULARI --------
+
+/*
+    alert() -> Kullanıcıya bir mesaj göstermek için kullanılır.
+    prompt() -> Kullanıcıdan bilgi almak için kullanılır.
+    confirm() -> Kullanıcıdan onay almak için kullanılır.
+*/
+
+// ! alert() -> Kullanıcıya bir mesaj göstermek için kullanılır.
+//   alert("Merhab, kayıt olun lütfen.");
+
+// ! prompt() -> Kullanıcıdan bilgi almak için kullanılır.
+//kullanıcıdan alınan değerler her zaman strıing olarak gelir.
+/*
+let isim = prompt("İsminiz nedir?");
+let yas = prompt("Yaşınız nedir?");
+console.log(isim, yas);
+*/
+
+// ! confirm() -> Kullanıcıdan onay almak için kullanılır.
+confirm("Emin misiniz silmek için?");
+
+//  let sonuc = confirm("Emin misiniz silmek için?");
+//  console.log(sonuc);
---
+++ 9.Koşul Yapıları/app.js
@@ -0,0 +1,43 @@
+// ? KOŞULLLAR --- CONDITIONALS
+
+/*
+let not = 65;
+if (not > 50) {
+  console.log("geçtiniz, notunuz : " + not);
+} else {
+  console.log("kaldınız, notunuz : " + not);
+}
+*/
+
+/*
+let yas = Number(prompt("Yaşınız : "));
+console.log(typeof yas);
+let para = Number(prompt("Paranız: "));
+
+if (yas >= 18 && para >= 3000) {
+  alert("Ehliyet Sınava katılabilirsiniz");
+} else {
+  alert("Ehliyet Sınava katılamazsınız");
+}
+*/
+
+// --------------- DERS ORTALAMA BULMA ----------------
+
+// vize1 %30, vize2 %30, final %40
+
+/*
+let vize1 = Number(prompt("Vize 1 notunuz giriniz : "));
+let vize2 = Number(prompt("Vize 2 notunuz giriniz : "));
+
+let final = Number(prompt("Final notunuz giriniz : "));
+
+let ortalama = vize1 * 0.3 + vize2 * 0.3 + final * 0.4;
+
+if (ortalama >= 60) {
+  alert("Tebrikler, dersi geçtiniz. Ortalamanız :) " + ortalama);
+  console.log("Tebrikler, dersi geçtiniz. Ortalamanız :) " + ortalama);
+} else {
+  alert("Dersten kaldınız. geçmiş olsun :() " + ortalama);
+  console.log("Dersten kaldınız. geçmiş olsun :() " + ortalama);
+}
+*/
