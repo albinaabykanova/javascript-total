358bdfa3570ca6d49614e7b4ec50adc3e3f41780
albinaabykanova <abykanovaalbina@gmail.com>
2023-06-15 13:56:59 +0300

    commit

---
+++ 10.Koşul Yapıları Else if Kullanımı/app.js
@@ -0,0 +1,29 @@
+//  ? ELSE IF KULLANIMI
+
+// else if kullanımı ile birden fazla koşul kontrolü yapılabilir.
+// else if olması için if bloğu olması gerekir.
+// else if bloğu sayısı istenilen kadar arttırılabilir.
+// else if bloğu sayısı sonlandıktan sonra else bloğu ile sonlandırılmalıdır.
+// if bloğu veya else if blokları true döndüğü zaman çalışır.
+// if den farklı olarak else if blokları arasında bir koşul sağlanırsa diğer else if blokları çalışmaz.
+/*
+   -1.yol 
+   -2.yol
+   -3.yol
+*/
+
+// ÖRNEK ////
+
+/*
+let secilenYol = prompt("Lütfen gitmek istediğiniz yolu seçiniz :");
+
+if (secilenYol == 1) {
+  alert("seçilen yol" + secilenYol + " yoludur.");
+} else if (secilenYol == 2) {
+  alert("seçilen yol" + secilenYol + " yoludur.");
+} else if (secilenYol == 3) {
+  alert("seçilen yol" + secilenYol + " yoludur.");
+} else {
+  alert("seçilen yol geçersizdir !!!");
+}
+*/
