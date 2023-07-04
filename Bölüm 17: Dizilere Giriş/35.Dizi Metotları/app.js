6ae36acbd99da2ae2b87e45e1414e2a5e370bb2d
albinaabykanova <abykanovaalbina@gmail.com>
2023-07-04 17:02:51 +0300

    daily  js commit

--- Bölüm 17: Dizilere Giriş/35.Dizi Metotları/app.js
+++ Bölüm 17: Dizilere Giriş/35.Dizi Metotları/app.js
@@ -15,11 +15,18 @@
 
    ---> concat() metodu: Dizileri birleştirir.
    ---> slice(dilimlemek) metodu: Dizinin belirli bir kısmını almak için kullanılır.
+
+   ---> reverse() metodu: Dizinin elemanlarını ters çevirir.
+   ---> split(bölmek) metodu: belirli bir ifadey7e göre bölüp diziye çevirir.
+
+   ---> indexOf() metodu: elemanın index numarasını verir.
+   ---> verilen elamanı içeriyor mu ona bakar, true veya false döner.
 */
 
 // TODO ORNEK 1
 
-let arabalar = ["BMW", "Mercedes", "Opel", "Mazda", "toyota", "mamalak"];
+//let arabalar1 = ["BMW", "Mercedes", "Opel", "Mazda", "toyota", "mamalak"];
+// let arabalar2 = ["lexus", "Ferrari", "Honda"];
 
 // ! push() metodu
 /*
@@ -61,6 +68,49 @@ console.log(stringArabalar);
 */
 
 // ! join() metodu
-
+/*
 let stringArabalar = arabalar.join(".................");
 console.log(stringArabalar);
+*/
+
+// ! concat() metodu
+/*
+let birlesmisDizi = arabalar1.concat(arabalar2);
+console.log(birlesmisDizi);
+*/
+
+// ! slice() metodu
+/*
+console.log(arabalar1);
+let baskaDizi = arabalar1.slice(2);
+console.log(baskaDizi);
+*/
+
+// ! reverse() metodu
+/*
+console.log(arabalar1);
+arabalar1.reverse();
+console.log(arabalar1);
+*/
+
+// ! split() metodu
+/*
+let isimler = "enes, talha, ahmet, mehmet, ayşe";
+let isimlerDizi = isimler.split(", ");
+console.log(isimlerDizi);
+*/
+
+// ! indexOf() metodu
+/*
+let index = arabalar1.indexOf("mamalak");
+if (index == 0) {
+  console.log("Aradığınız değer dizinin ilk elemanıdır.");
+} else {
+  console.log("elaman yoktur");
+}
+*/
+
+/*
+let sonuc = arabalar1.includes("audi");
+console.log(sonuc);
+*/
