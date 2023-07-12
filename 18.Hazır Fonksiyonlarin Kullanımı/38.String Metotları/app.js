185a4a63467affc01b58e3bf95f09be76041f1db
albinaabykanova <abykanovaalbina@gmail.com>
2023-07-12 23:47:56 +0300

    daily js pratic commit

---
+++ 18.Hazır Fonksiyonlarin Kullanımı/38.String Metotları/app.js
@@ -0,0 +1,104 @@
+// ? STRİNG SINIFININ METOTLARI
+
+// let kurs = "Modern Web Geliştirme Kursu";
+// let tarih = "2023";
+
+// chartAt()
+// concat()
+// indexOf()
+// lastIndexOf()
+// toUpperCase()
+// toLowerCase()
+// trim()
+// slice()
+// substring()
+// replace()
+// split()
+// valueOf()
+// startWith()
+// endWith()
+
+// !  charAt() kullanım metotu
+/*
+let karakter = kurs.charAt(0);
+console.log(karakter);
+*/
+
+// ! concat() kullanım metotu
+/*
+let sonuc = kurs.concat(" ", tarih, " ense");
+console.log(sonuc);
+*/
+
+// ! indexOf() kullanım metotu
+/*
+let index = kurs.indexOf("W");
+console.log(index);
+*/
+
+/*
+let index = kurs.lastIndexOf("Kursu");
+console.log(index);
+*/
+
+// ! toUpperCase() metot kullanımı
+/*
+let sonuc = kurs.toLocaleUpperCase();
+console.log(sonuc);
+console.log(kurs);
+*/
+
+// ! toLowerCase() metot kullanımı
+/*
+let sonuc = kurs.toLocaleLowerCase();
+console.log(sonuc);
+console.log(kurs);
+*/
+
+// ! trim() metot kullanımı
+/*
+kurs.trim();
+console.log(kurs);
+*/
+
+// ! slice() kullanım metotu
+/*
+console.log(kurs);
+console.log(kurs.slice(11, 19));
+*/
+
+// ! Substring() kullanım metotu
+/*
+console.log(kurs);
+console.log(kurs.substring(0, 6));
+*/
+
+// ! replac() kullanım metotu
+/*
+console.log(kurs);
+console.log(kurs.replace("Modern", "Mamalak"));
+*/
+
+// ! split() kullanım metotu
+/*
+console.log(kurs);
+let dizi = kurs.split(" ");
+console.log(dizi);
+*/
+
+// ! valueOf() kullanım metotu
+/*
+console.log(kurs.valueOf());
+*/
+
+// ! startsWith() kullanım metotu
+/*
+console.log(kurs);
+console.log(kurs.startsWith("M"));
+*/
+
+// ! endWith() kullanım metotu
+/*
+console.log(kurs);
+console.log(kurs.endsWith("o"));
+*/
