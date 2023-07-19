6380b9192b746ae43cf79bcaaf134ad5859cefdb
albinaabykanova <abykanovaalbina@gmail.com>
2023-07-19 23:21:45 +0300

    daily commit

---
+++ Bölüm 18.Hazır Fonksiyonlarin Kullanımı/39.Math Metotları/app.js
@@ -0,0 +1,25 @@
+// ? MATH SINIFI METOTLAR
+
+/*
+1-Floor
+2-ceil 
+3-Round 
+
+4-Max 
+5-Min 
+
+6-Random 
+7-Abs 
+8-Sqrt 
+9-Pow 
+*/
+
+let a = 3.99;
+/*
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
+console.log(Math.round(a));
+*/
+
+//console.log(Math.max(33, 5, 78));
+console.log(Math.min(3, 0, 12));
