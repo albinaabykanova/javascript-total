4bb52ecf77c3b097b2e621c2cd487c669dec8793
albinaabykanova <abykanovaalbina@gmail.com>
2023-07-22 23:42:14 +0300

    daily commit js

--- Bölüm 18.Hazır Fonksiyonlarin Kullanımı/39.Math Metotları/app.js
+++ Bölüm 18.Hazır Fonksiyonlarin Kullanımı/39.Math Metotları/app.js
@@ -9,17 +9,19 @@
 5-Min 
 
 6-Random 
-7-Abs 
+7-Abs - mutlak değer almak için kullanılır |-12| = 12 
 8-Sqrt 
 9-Pow 
 */
 
 let a = 3.99;
+let b = -12;
 /*
 console.log(Math.floor(a));
 console.log(Math.ceil(a));
 console.log(Math.round(a));
 */
 
-//console.log(Math.max(33, 5, 78));
-console.log(Math.min(3, 0, 12));
+// console.log(Math.max(33, 5, 78));
+// console.log(Math.min(3, 0, 12));
+console.log(Math.abs(b));
