3a329c85112199d584ba06b17ff9886c1f27ee83
albinaabykanova <abykanovaalbina@gmail.com>
2023-07-24 22:12:43 +0300

    daily commit js

--- Bölüm 18.Hazır Fonksiyonlarin Kullanımı/39.Math Metotları/app.js
+++ Bölüm 18.Hazır Fonksiyonlarin Kullanımı/39.Math Metotları/app.js
@@ -11,11 +11,15 @@
 6-Random 
 7-Abs - mutlak değer almak için kullanılır |-12| = 12 
 8-Sqrt - karekök almak için kullanılır
-9-Pow 
+9-Pow - üssünü almak için kullanılır
+-----> Pİ SAYİSİ
 */
 
+/*
 let a = 3.99;
 let b = -12;
+*/
+
 /*
 console.log(Math.floor(a));
 console.log(Math.ceil(a));
@@ -26,4 +30,17 @@ console.log(Math.round(a));
 // console.log(Math.min(3, 0, 12));
 // console.log(Math.abs(b));
 
-console.log(Math.sqrt(46));
+// console.log(Math.sqrt(46));
+// console.log(Math.pow(8, 2));
+
+// console.log(Math.PI);
+
+/*
+let randomDeger = Math.random();
+let sonuc = randomDeger * 100;
+let sonucYuvarla = Math.floor(sonuc);
+
+console.log(randomDeger);
+console.log(sonuc);
+console.log(sonucYuvarla);
+*/
