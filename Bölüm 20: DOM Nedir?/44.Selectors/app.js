819b376cbc30380b65be34e64e5c04df2e87e9a1
albinaabykanova <abykanovaalbina@gmail.com>
2023-08-03 13:04:29 +0300

    daily js commit

---
+++ Bölüm 20: DOM Nedir?/44.Selectors/app.js
@@ -0,0 +1,13 @@
+// ? SELECTORS (SEÇİCİLER) - STYLE ÖZELLİKLER
+
+//classname , id, tag name
+
+//querySelector - querySelectorsAll
+
+/*
+todoList.forEach(function (todo) {
+  todo.style.backgroundColor = "pink";
+});
+
+console.log(todoList);
+*/
---
+++ Bölüm 20: DOM Nedir?/45.Style Özelliği/app.js
@@ -0,0 +1,5 @@
+// ? STYLE ÖZELLİĞİ
+
+const todo = document.querySelector("list-group-item");
+
+console.log(todo);
