6f2afffaed5c185cb29dcd0e3ab24225c22c47eb
albinaabykanova <abykanovaalbina@gmail.com>
2023-08-13 23:11:13 +0300

    daily js commit

--- Bölüm 20: DOM Nedir?/48.Element Silme/app.js
+++ Bölüm 20: DOM Nedir?/48.Element Silme/app.js
@@ -1,6 +1,16 @@
 // ? ELEMENT SİLME
 
+/*
 const todoList = document.querySelector(".list-group");
-const todos = querySelectorAll(".list-group-item");
+const todos = document.querySelectorAll(".list-group-item");
+const todo1 = document.querySelector(".list-group-item");
+*/
 
-console.log(todos);
+// todos[0].remove();
+// todos[1].remove();
+// todos[2].remove();
+// console.log(todos);
+
+// todos[todos.length - 1].remove();
+
+//todo1.remove();
---
+++ Bölüm 20: DOM Nedir?/49.Element Değiştirmek/app.js
@@ -0,0 +1,10 @@
+// ? ELEMENT DEĞİŞTİRMEK
+/*
+const cardBody = document.querySelectorAll(".card-body")[1];
+
+const newTitle = document.createElement("h2");
+newTitle.className = "card-title";
+newTitle.textContent = "Todo Listesi - Yeni";
+
+cardBody.replaceChild(newTitle, cardBody.childNodes[1]);
+*/
