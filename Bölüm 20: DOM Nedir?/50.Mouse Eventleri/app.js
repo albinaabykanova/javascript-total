04d0c125d41e6e4f649e9f304f96a4e785b6b8cc
albinaabykanova <abykanovaalbina@gmail.com>
2023-08-14 16:48:45 +0300

    daily js commit

---
+++ Bölüm 20: DOM Nedir?/50.Mouse Eventleri/app.js
@@ -0,0 +1,40 @@
+// ?   EVENT (OLAYLAR)
+
+/*
+const clearButton = document.querySelector("#todoClearButton");
+clearButton.addEventListener("click", changeTitle);
+
+function changeTitle(event) {
+  document.querySelectorAll(".card-title")[1].textContent =
+    "Todo Başlığı Değiştir lllllllllllllllll";
+}
+*/
+
+// ! MOUSE EVENTLARI
+
+// DOMContentLoaded
+// load
+// click
+// dblclick
+// mouseover
+// mouseout
+// mouseenter
+// mousedown
+
+/*
+document.addEventListener("DOMContentLoaded", run);
+function run() {
+  console.log("Sayfa Yüklendi");
+}
+*/
+
+/*
+const cardTitle = document.querySelectorAll(".card-title")[1];
+const cardBody = document.querySelectorAll(".card-body")[1];
+
+cardBody.addEventListener("mouseover", run);
+
+function run(e) {
+  console.log(e.type);
+}
+*/
---
+++ Bölüm 20: DOM Nedir?/51.Klaviye Eventları/app.js
@@ -0,0 +1,15 @@
+// ? KLAVİYE EVENTLARI
+
+//keypress - harf ve sayılarda tetiklenen eventtir....
+
+//keydown - hepsinde çalışır
+
+//keyup - tuştan elini kaldırdığında çalışan eventtir
+
+/*
+document.addEventListener("keypress", run);
+
+function run(e) {
+  console.log(e.type);
+}
+*/
