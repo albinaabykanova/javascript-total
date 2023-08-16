1b2b8fcbd3fcd61025dd86140308a23bb63f048f
albinaabykanova <abykanovaalbina@gmail.com>
2023-08-16 13:56:27 +0300

    daily commit

--- Bölüm 22: Stroge Kavramı/53. Stroge/app.js
+++ Bölüm 22: Stroge Kavramı/53. Stroge/app.js
@@ -1,5 +1,6 @@
 // ? SESSİON  STROGE KAVRAMI
 
+/*
 sessionStorage.setItem("350", "enes");
 sessionStorage.setItem("234", "david");
 sessionStorage.setItem("23", "dede");
@@ -10,5 +11,30 @@ if (value === null) {
 } else {
   console.log("Değer bulundu :", value);
 }
+*/
 
 // sessionStorage.clear();
+
+/*
+let names = ["ali", "enes", "kübra", "adem"];
+sessionStorage.setItem("names", JSON.stringify(names));
+
+let value = JSON.parse(sessionStorage.getItem("names"));
+value.forEach(function (name) {
+  console.log(name);
+});
+*/
+
+// ? LOCAL STROGE
+
+/*
+localStorage.setItem("motion1", "Push up");
+localStorage.setItem("motion2", "Barfix");
+localStorage.setItem("motion3", "Burpee");
+localStorage.setItem("motion4", "Squat");
+
+let value = localStorage.getItem("motion1");
+console.log(value);
+
+localStorage.removeItem("motion4");
+*/
